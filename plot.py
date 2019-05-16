e8a19be420099df7eaaf1613c0bf8e385c0f699c
spnutz <nutty1997@live.com>
2019-05-16 11:03:28 +0700

    show graph into GUI

---
+++ plot.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+class GraphGui(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.top_fram = tk.Frame(self).pack(side = "top")
+        self.bottom_fram = tk.Frame(self).pack(side = "bottom")
+
+       
+        tk.Label(self.bottom_fram,text="x1 :").pack()
+        self.x1 = tk.Entry(self.bottom_fram)
+        self.x1.pack()
+        
+        tk.Label(self.bottom_fram,text="y1 :").pack()
+        self.y1 = tk.Entry(self.bottom_fram)
+        self.y1.pack()
+
+        tk.Label(self.bottom_fram,text="x2 :").pack()
+        self.x2 = tk.Entry(self.bottom_fram)
+        self.x2.pack()
+
+        tk.Label(self.bottom_fram,text="y2 :").pack()
+        self.y2 = tk.Entry(self.bottom_fram)
+        self.y2.pack()
+
+        self.button = tk.Button(self.bottom_fram, text="Plot", command=self.getData)
+        self.button.pack(side='left')
+
+        self.btn_exit = tk.Button(self.bottom_fram, text='Quit', command=self.quit)
+        self.btn_exit.pack(side='right')     
+       
+
+    def getData(self):
+        data_x1 = float(self.x1.get())
+        data_y1 = float(self.y1.get())
+        data_x2 = float(self.x2.get())
+        data_y2 = float(self.y2.get())
+
+        print("x1: {} y1: {}\nx2: {} y2: {}".format(data_x1, data_y1, data_x2, data_y2))
+        self.plot_gui(data_x1, data_y1, data_x2, data_y2)
+        
+    def plot_gui(self,x1,y1,x2,y2):
+        fig = Figure(figsize=(6,6))
+        a = fig.add_subplot(111)
+        a.plot([x1,y1,x2,y2])
+
+        canvas = FigureCanvasTkAgg(fig, master=self.top_fram)
+        canvas.get_tk_widget().pack(side="right")
+        canvas.draw()
+
+
+app = GraphGui()
+app.mainloop()
