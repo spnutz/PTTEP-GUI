1958b8229ac6804702e4ab69f946010d51bc414d
spnutz <nutty1997@live.com>
2019-05-17 13:29:20 +0700

    use LabelFrame to GUI

--- plot.py
+++ plot.py
@@ -2,81 +2,93 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import time
-import threading
+from tkinter import ttk
+from tkinter import Menu
 
-class GraphGui(tk.Tk):
+class Graph(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.geometry("600x600") # set size window
-        self.resizable(False,False) # disable re-size
         
-        self.continuePlot = False
+        # Create container 
+        self.big_frame = ttk.LabelFrame(self, text="Big Frame")
+        self.big_frame.grid(column=0, row=0)
 
-        self.top_fram = tk.Frame(self)
-        self.top_fram.pack(side = "top")
-        self.bottom_fram = tk.Frame(self)
-        self.bottom_fram.pack(side = "bottom")
+        # Frame input
+        self.frame1 = ttk.LabelFrame(self.big_frame, text="frame1")
+        self.frame1.grid(row=0, column=1)
 
-        tk.Label(self.bottom_fram,text="u :").pack()
-        self.u = tk.Entry(self.bottom_fram)
-        self.u.pack()
-        
-        tk.Label(self.bottom_fram,text="a :").pack()
-        self.a = tk.Entry(self.bottom_fram)
-        self.a.pack()
+        # Frame Figure
+        self.frame2 = ttk.LabelFrame(self.big_frame, text="Figure Frame")
+        self.frame2.grid(row=0, column=0)
 
-        tk.Label(self.bottom_fram,text="t :").pack()
-        self.t = tk.Entry(self.bottom_fram)
-        self.t.pack()
+        ###### Add widget ######
 
-        #tk.Label(self.bottom_fram,text="y2 :").pack()
-        #self.y2 = tk.Entry(self.bottom_fram)
-        #self.y2.pack()
+        #### input box  u#####
+        tk.Label(self.frame1, text='u :').grid(row=0, column=0)
+        self.entry_u = tk.Entry(self.frame1, bd=3, width=5)
+        self.entry_u.grid(row=0, column=1)
 
-        self.button = tk.Button(self.bottom_fram, text="Plot", command=self.getData)
-        self.button.pack(side='left')
+        tk.Label(self.frame1, text='a :').grid(row=1, column=0)
+        self.entry_a = tk.Entry(self.frame1, bd=3, width=5)
+        self.entry_a.grid(row=1, column=1)
 
-        self.btn_exit = tk.Button(self.bottom_fram, text='Qut', command=self.quit)
-        self.btn_exit.pack(side='right')     
-       
+        tk.Label(self.frame1, text='t :').grid(row=2, column=0)
+        self.entry_t = tk.Entry(self.frame1, bd=3, width=5)
+        self.entry_t.grid(row=2, column=1)
+
+        # Button plot
+        self.btn_plot = tk.Button(self.frame1, text="Plot", command=self.getData, width=8)
+        self.btn_plot.grid(row=4, column=1)
+
+        # fig 
+        self.fig = Figure(figsize=(4, 4))
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame2)
+        self.canvas.get_tk_widget().grid(row=0, column=0)
+        self.canvas.draw()
+
+        # Menu Bar
+        self.menu_bar = Menu(self)
+        self.config(menu=self.menu_bar)
+        self.create_menu_bar()
+ 
+    #### get data from input #####
 
     def getData(self):
-        data_u = float(self.u.get())
-        data_a = float(self.a.get())
-        data_t = float(self.t.get())
-        #data_y2 = float(self.y2.get())
+        u = float(self.entry_u.get())
+        a = float(self.entry_a.get())
+        t = float(self.entry_t.get())
 
-        print("u: {}\na: {}\nt: {} ".format(data_u, data_a, data_t))
-        self.calculate(data_u, data_a, data_t)
+        # calculate
+        s = float((u*t) + (0.5*t*t))
+        print('u: {}\na: {}\nt: {}\ns: {}\n'.format(u, a, t, s))
+        self.plot_graph(s, t)
 
+    #####    Plot   #####
 
-    def calculate(self,u,a,t):
-        s = (u*t) + (0.5*a*t*t)
-        print("s:",s)
-        self.change_state()
-        self.plot_gui(s,t)
-
-        
-    def plot_gui(self,s,t):
-        
-        fig = Figure(figsize=(4,4))
-        a = fig.add_subplot(111)
+    def plot_graph(self,s,t):
+        a = self.fig.add_subplot(111)
+        a.clear()
         a.plot([t,s])
+        self.canvas.draw()
 
-        canvas = FigureCanvasTkAgg(fig, master=self.top_fram)
-        canvas.get_tk_widget().pack()
-        canvas.draw()
+    #####   Menu Bar  ######
+
+    def create_menu_bar(self):
+        file_menu = Menu(self.menu_bar, tearoff=0)
+        file_menu.add_command(label="Exit", command=self._quit)
+        self.menu_bar.add_cascade(label="File", menu=file_menu)
+
+   ##### exit function menu bar #####
+
+    def _quit(self):
+        self.quit()
+        self.destroy()
+        exit()
+
         
-    def change_state(self):
-        if self.continuePlot == True:
-            self.continuePlot = False
-        else:
-            self.continuePlot = True
-
-
-    
-
-
-app = GraphGui()
-app.mainloop()
+####################
+#     STRAT GUI    #
+####################
+if __name__ == "__main__":
+    app = Graph()
+    app.mainloop()
